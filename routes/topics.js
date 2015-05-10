21edfa50da637ce6e594426427fd9ad019e4b6ca
pauge <arto.kiviluoto@cs.helsinki.fi>
2015-05-10 20:03:56 +0300

    Task 50

--- routes/topics.js
+++ routes/topics.js
@@ -4,37 +4,44 @@ var router = express.Router();
 var authentication = require('../utils/authentication');
 var Models = require('../models');
 
-// Huom! Kaikki polut alkavat polulla /topics
 
-// GET /topics
+
+
 router.get('/', function(req, res, next) {
-    // Hae kaikki aihealueet tässä (Vinkki: findAll)
-    res.send(200);
+    Models.Topic.findAll().then(function(topics) {
+        res.json(topics);
+    })
 });
 
-// GET /topics/:id
+
 router.get('/:id', function(req, res, next) {
-  // Hae aihealue tällä id:llä tässä (Vinkki: findOne)
   var topicId = req.params.id;
-  res.send(200);
+  Models.Topic.findOne(topicId)
+            .then(function(topic) {
+                res.json(topic);
+              })
 });
 
-// POST /topics
+
 router.post('/', function(req, res, next) {
-  // Lisää tämä aihealue
   var topicToAdd = req.body;
-  // Palauta vastauksena lisätty aihealue
-  res.send(200);
+  Models.Topic.create({name:topicToAdd.name,description:topicToAdd.description})
+            .then(function(topic) {
+                res.json(topic);
+            })
 });
 
-// POST /topics/:id/message
+
 router.post('/:id/message', function(req, res, next) {
-  // Lisää tällä id:llä varustettuun aihealueeseen...
   var topicId = req.params.id;
   // ...tämä viesti (Vinkki: lisää ensin messageToAdd-objektiin kenttä TopicId, jonka arvo on topicId-muuttujan arvo ja käytä sen jälkeen create-funktiota)
   var messageToAdd = req.body;
-  // Palauta vastauksena lisätty viesti
-  res.send(200);
+  messageToAdd.topicId = topicId;
+  console.log(messageToAdd)
+    Models.Topic.create(messageToAdd)
+                .then(function(message) {
+                    res.json(message)
+                })
 });
 
 module.exports = router;
