aca0837247e3ff576b3b6c059ca92f9db23b816c
pauge <arto.kiviluoto@cs.helsinki.fi>
2015-05-10 21:27:15 +0300

    Task 51

--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -1,3 +1,15 @@
 FoorumApp.controller('TopicsListController', function($scope, $location, Api){
-  // Toteuta kontrolleri tähän
+  $scope.newTopic;
+  Api.getTopics().success(function(topics) {
+      $scope.topics = topics;
+  });
+  
+  $scope.addTopic = function() {
+      Api.addTopic($scope.newTopic).success(function(data) {
+            console.log(data)
+            $location.path('/topics/'+data.id)
+      }).error(function(error) {
+          console.log(error)
+      })
+  }
 });
